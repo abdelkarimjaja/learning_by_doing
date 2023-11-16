b72f84c79b1fb345a5929fbe251157e57a04bda8
abdelkarimjaja <abdelkarimjaja1@gmail.com>
2023-11-16 19:54:32 +0100

    some intereseting projects

---
+++ livewire-projects/livewire-quick-start/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<html> 
+
+<head>
+
+<title>Livewire Screencasts</title>
+
+</head>
+
+<body>
+
+<!--livewire:counter /--> 
+<livewire:hello-world /> 
+
+
+</body>
+</html>
+
--- reacts-projects/tic-tac-toe/src/components/Square.jsx
+++ reacts-projects/tic-tac-toe/src/components/Square.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
+function Square({value, onSquareClick}) {
+	// const [value, setValue] = useState(null);
 
-function Square({value}) {
-	  function handleClick() {
-		      console.log('clicked!');
-		    }
+// 	function handleClick() {
+// 		setValue('X');
+// 	}
 
-  return (
-	          <button className="square"
-	        onClick={handleClick}
+		return <button 
+			className="square"
+			onClick = {onSquareClick}
+			>
+			{value}
+			</button>;
 
-		  >{value}</button>
-	    );
+	
 }
 
 export default Square
